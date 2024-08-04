d5306c7818211fa272b1dd134da7b5fbd5295e56
Ira S <iryna.shevchenko2@gmail.com>
2024-08-04 16:30:45 +0300

    unit testing for auth flow

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -19,13 +19,13 @@ import { NotFoundException } from '@nestjs/common/exceptions';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 // import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { User } from './user.entity';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 // if I want to use interceptor only for all routes, place it here:
